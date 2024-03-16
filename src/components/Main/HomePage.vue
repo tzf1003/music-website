808f07a57a759ab45f26369a1ca0103c9eead0fb
落花人 <1244201693@qq.com>
2024-03-16 12:57:50 +0800

    增加判断是否展示主页的一些内容

--- src/components/Main/HomePage.vue
+++ src/components/Main/HomePage.vue
@@ -3,7 +3,7 @@
     <div class="content top-historys">
 
       <div class="title history" style="font-size: 32px;">
-        <span>{{greeting}} {{ username }}</span>
+        <span>{{ greeting }} {{ username }}</span>
       </div>
       <div class="historys">
         <el-row :gutter="10">
@@ -52,16 +52,15 @@
         </el-row>
       </div>
     </div>
-    <ScrollableRow title="热门推荐" :items="hotsItems" />
-    <!-- 测试数据 -->
-    <ScrollableRow title="猜你喜欢" :items="likeItems"  />
-    <ScrollableRow title="歌手推荐" :items="singersItems"/>
-    <ScrollableRow title="历史播放" :items="historyItems"/>
+    <ScrollableRow v-if="hotsItems.length!=0" title="热门推荐" :items="hotsItems" />
+    <ScrollableRow v-if="likeItems.length!=0" title="猜你喜欢" :items="likeItems" />
+    <ScrollableRow v-if="singersItems.length!=0" title="歌手推荐" :items="singersItems" />
+    <ScrollableRow v-if="historyItems.length!=0" title="历史播放" :items="historyItems" />
   </div>
 </template>
 
 <script setup>
-import { ref,computed  } from "vue";
+import { ref, computed } from "vue";
 import ScrollableRow from "./ScrollableRow.vue"
 import apiService from '@/tools/apiService';
 
@@ -72,8 +71,8 @@ const singer = ref(null);
 const hotsItems = ref([]);
 const likeItems = ref([]);
 const singersItems = ref([]);
-const historyItems= ref([]);
-const username=ref('')
+const historyItems = ref([]);
+const username = ref('')
 // 创建一个响应式数据来存储当前的时间
 const currentTime = ref(new Date());
 // 计算属性，根据当前时间返回不同的问候语
@@ -91,7 +90,7 @@ const greeting = computed(() => {
 });
 //获取用户名
 var userInfo = JSON.parse(localStorage.getItem('userInfo'));
-if(userInfo!=null){
+if (userInfo != null) {
   username.value = userInfo.username;
 }
 
@@ -113,49 +112,49 @@ const homeData = apiService.fetchWithAuth("home", "GET", null).then((result) => 
     };
     hotsItems.value.push(list); // 使用push方法添加元素到数组中
   }
-   //设置猜你喜欢
-   likeItems.value.push({
-      name: like.value[0].name,
-      description: like.value[0].description,
-      id: like.value[0].id,
-      imgUrl: like.value[0].imgUrl,
-      type: "sheet"
-    });
-    likeItems.value.push({
-      name: like.value[1].name,
-      description: like.value[1].description,
-      id: like.value[1].id,
-      imgUrl: like.value[1].imgUrl,
-      type: "sheet"
-    });
-    likeItems.value.push({
-      name: like.value[2].name,
-      description: like.value[2].description,
-      id: like.value[2].id,
-      imgUrl: like.value[2].imgUrl,
-      type: "singer"
-    });
-    likeItems.value.push({
-      name: like.value[3].name,
-      description: like.value[3].description,
-      id: like.value[3].id,
-      imgUrl: like.value[3].imgUrl,
-      type: "singer"
-    });
-    likeItems.value.push({
-      name: like.value[4].name,
-      description: like.value[4].description,
-      id: like.value[4].id,
-      imgUrl: like.value[4].imgUrl,
-      type: "music"
-    });
-    likeItems.value.push({
-      name: like.value[5].name,
-      description: like.value[5].description,
-      id: like.value[5].id,
-      imgUrl: like.value[5].imgUrl,
-      type: "music"
-    });
+  //设置猜你喜欢
+  likeItems.value.push({
+    name: like.value[0].name,
+    description: like.value[0].description,
+    id: like.value[0].id,
+    imgUrl: like.value[0].imgUrl,
+    type: "sheet"
+  });
+  likeItems.value.push({
+    name: like.value[1].name,
+    description: like.value[1].description,
+    id: like.value[1].id,
+    imgUrl: like.value[1].imgUrl,
+    type: "sheet"
+  });
+  likeItems.value.push({
+    name: like.value[2].name,
+    description: like.value[2].description,
+    id: like.value[2].id,
+    imgUrl: like.value[2].imgUrl,
+    type: "singer"
+  });
+  likeItems.value.push({
+    name: like.value[3].name,
+    description: like.value[3].description,
+    id: like.value[3].id,
+    imgUrl: like.value[3].imgUrl,
+    type: "singer"
+  });
+  likeItems.value.push({
+    name: like.value[4].name,
+    description: like.value[4].description,
+    id: like.value[4].id,
+    imgUrl: like.value[4].imgUrl,
+    type: "music"
+  });
+  likeItems.value.push({
+    name: like.value[5].name,
+    description: like.value[5].description,
+    id: like.value[5].id,
+    imgUrl: like.value[5].imgUrl,
+    type: "music"
+  });
 
   //设置歌手推荐
   for (var singer1 of singer.value) { // 使用for...of循环遍历数组元素
@@ -169,7 +168,8 @@ const homeData = apiService.fetchWithAuth("home", "GET", null).then((result) => 
     singersItems.value.push(list); // 使用push方法添加元素到数组中
   }
   //历史播放
-  for (var history1 of history.value) { // 使用for...of循环遍历数组元素
+  if (history.value != null) {
+    for (var history1 of history.value) { // 使用for...of循环遍历数组元素
     let list = {
       name: history1.hisObj.name,
       description: history1.hisObj.description,
@@ -179,6 +179,10 @@ const homeData = apiService.fetchWithAuth("home", "GET", null).then((result) => 
     };
     historyItems.value.push(list); // 使用push方法添加元素到数组中
   }
+  }else{
+    historyItems.value=[];
+  }
+
 });
 const handle = (type, id) => {
   this.$router.push('/' + type + '/' + id);
